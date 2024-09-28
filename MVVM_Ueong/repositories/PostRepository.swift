45fd506b6394e3aacbdb03549578342e8d12f870
KimAman <chalgaro46@gmail.com>
2024-09-28 21:32:34 +0900

    1

--- MVVM_Ueong/repositories/PostRepository.swift
+++ MVVM_Ueong/repositories/PostRepository.swift
@@ -12,8 +12,37 @@ class PostRepository {
         // MySQL에서 데이터 받아오는 로직
         
         let posts = [
-            Post(id:1, title:"자전거", price:3000, isFavorite: false, text: "hahahah"),
-            Post(id:2, title:"자전거", price:3000, isFavorite: false, text: "soososososo")
+           Post(
+                id: 9,
+                title: "자전거",
+                category: 1,
+                price: 3000.0,
+                emdId: 1,
+                latitude: 37.5,
+                longitude: 160.0,
+                locationDetail: "아무데나",
+                createAt: Date(), // Date()로 초기화
+                isFavorite: false,
+                text: "soososososo",
+                photos: [
+                    Photo(id: 1, postId: 1, url: "cat1.jpg") // 여기에 사진의 URL을 적어주세요
+                ]
+            ), Post(
+                id: 9,
+                title: "자전거",
+                category: 1,
+                price: 3000.0,
+                emdId: 1,
+                latitude: 37.5,
+                longitude: 160.0,
+                locationDetail: "아무데나",
+                createAt: Date(), // Date()로 초기화
+                isFavorite: false,
+                text: "soososososo",
+                photos: [
+                    Photo(id: 1, postId: 1, url: "cat1.jpg") // 여기에 사진의 URL을 적어주세요
+                ]
+            )
         ]
         return posts
     }
@@ -22,19 +51,38 @@ class PostRepository {
         // MySQL에서 데이터 받아오는 로직
         
         let posts = [
-            Post(id:3, title:"자전거", price:3000, isFavorite: false, text: "gogogogoogog"),
-            Post(id:4, title:"자전거", price:3000, isFavorite: false, text:" hohohohohoh")
-        ]
-        return posts
-    }
-    
-    func getPostList() -> [Post] {
-        // MySQL에서 데이터 받아오는 로직
-        
-        let posts = [
-            Post(id:5, title:"자전거", price:3000, isFavorite: false, text: "hahahah"),
-            Post(id:6, title:"자전거", price:3000, isFavorite: false, text: "soososososo")
-        ]
+            Post(
+                 id: 9,
+                 title: "자전거",
+                 category: 1,
+                 price: 3000.0,
+                 emdId: 1,
+                 latitude: 37.5,
+                 longitude: 160.0,
+                 locationDetail: "아무데나",
+                 createAt: Date(), // Date()로 초기화
+                 isFavorite: false,
+                 text: "soososososo",
+                 photos: [
+                     Photo(id: 1, postId: 1, url: "cat1.jpg") // 여기에 사진의 URL을 적어주세요
+                 ]
+             ), Post(
+                 id: 9,
+                 title: "자전거",
+                 category: 1,
+                 price: 3000.0,
+                 emdId: 1,
+                 latitude: 37.5,
+                 longitude: 160.0,
+                 locationDetail: "아무데나",
+                 createAt: Date(), // Date()로 초기화
+                 isFavorite: false,
+                 text: "soososososo",
+                 photos: [
+                     Photo(id: 1, postId: 1, url: "cat1.jpg") // 여기에 사진의 URL을 적어주세요
+                 ]
+             )
+         ]
         return posts
     }
     
@@ -51,13 +99,65 @@ class PostRepository {
             }
     }
     
+    func getPostDetail(userId:Int) -> Post {
+        // MySQL에서 데이터 받아오는 로직
+        
+        let posts = Post(
+            id: 9,
+            title: "자전거",
+            category: 1,
+            price: 3000.0,
+            emdId: 1,
+            latitude: 37.5,
+            longitude: 160.0,
+            locationDetail: "아무데나",
+            createAt: Date(), // Date()로 초기화
+            isFavorite: false,
+            text: "soososososo",
+            photos: [
+                Photo(id: 1, postId: 1, url: "cat1.jpg") // 여기에 사진의 URL을 적어주세요
+            ]
+        )
+      
+        return posts
+    }
+    
     func getFavoriteList(userId:Int) -> [Post] {
         // MySQL에서 데이터 받아오는 로직
         
         let posts = [
-            Post(id:8, title:"자전거", price:3000, isFavorite: false, text: "hahahah"),
-            Post(id:9, title:"자전거", price:3000, isFavorite: false, text: "soososososo")
-        ]
+            Post(
+                 id: 9,
+                 title: "자전거",
+                 category: 1,
+                 price: 3000.0,
+                 emdId: 1,
+                 latitude: 37.5,
+                 longitude: 160.0,
+                 locationDetail: "아무데나",
+                 createAt: Date(), // Date()로 초기화
+                 isFavorite: false,
+                 text: "soososososo",
+                 photos: [
+                     Photo(id: 1, postId: 1, url: "cat1.jpg") // 여기에 사진의 URL을 적어주세요
+                 ]
+             ), Post(
+                 id: 9,
+                 title: "자전거",
+                 category: 1,
+                 price: 3000.0,
+                 emdId: 1,
+                 latitude: 37.5,
+                 longitude: 160.0,
+                 locationDetail: "아무데나",
+                 createAt: Date(), // Date()로 초기화
+                 isFavorite: false,
+                 text: "soososososo",
+                 photos: [
+                     Photo(id: 1, postId: 1, url: "cat1.jpg") // 여기에 사진의 URL을 적어주세요
+                 ]
+             )
+         ]
         return posts
     }
 }
