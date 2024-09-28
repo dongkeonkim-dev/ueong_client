d28c08b597a8e39499c9a5e630c610bec533733b
KimAman <chalgaro46@gmail.com>
2024-09-28 21:34:24 +0900

    1

--- MVVM_Ueong/Repositories/PostRepository.swift
+++ MVVM_Ueong/Repositories/PostRepository.swift
@@ -12,8 +12,37 @@ class PostRepository {
         // MySQL에서 데이터 받아오는 로직
         
         let posts = [
-            Post(id:1, title:"자전거", price:3000, isFavorite: false, text: "hahahah"),
-            Post(id:2, title:"자전거", price:3000, isFavorite: false, text: "soososososo")
+           Post(
+                id: 9,
+                title: "자전거",
+                category: 1,
+                price: 3000.0,
+                emdId: 1,
+                latitude: 37.5,
+                longitude: 160.0,
+                locationDetail: "아무데나",
+                createAt: Date(), // Date()로 초기화
+                isFavorite: false,
+                text: "soososososo",
+                photos: [
+                    Photo(id: 1, postId: 1, url: "cat1.jpg") // 여기에 사진의 URL을 적어주세요
+                ]
+            ), Post(
+                id: 9,
+                title: "자전거",
+                category: 1,
+                price: 3000.0,
+                emdId: 1,
+                latitude: 37.5,
+                longitude: 160.0,
+                locationDetail: "아무데나",
+                createAt: Date(), // Date()로 초기화
+                isFavorite: false,
+                text: "soososososo",
+                photos: [
+                    Photo(id: 1, postId: 1, url: "cat1.jpg") // 여기에 사진의 URL을 적어주세요
+                ]
+            )
         ]
         return posts
     }
@@ -22,26 +51,73 @@ class PostRepository {
         // MySQL에서 데이터 받아오는 로직
         
         let posts = [
-            Post(id:3, title:"자전거", price:3000, isFavorite: false, text: "gogogogoogog"),
-            Post(id:4, title:"자전거", price:3000, isFavorite: false, text:" hohohohohoh")
-        ]
+            Post(
+                 id: 9,
+                 title: "자전거",
+                 category: 1,
+                 price: 3000.0,
+                 emdId: 1,
+                 latitude: 37.5,
+                 longitude: 160.0,
+                 locationDetail: "아무데나",
+                 createAt: Date(), // Date()로 초기화
+                 isFavorite: false,
+                 text: "soososososo",
+                 photos: [
+                     Photo(id: 1, postId: 1, url: "cat1.jpg") // 여기에 사진의 URL을 적어주세요
+                 ]
+             ), Post(
+                 id: 9,
+                 title: "자전거",
+                 category: 1,
+                 price: 3000.0,
+                 emdId: 1,
+                 latitude: 37.5,
+                 longitude: 160.0,
+                 locationDetail: "아무데나",
+                 createAt: Date(), // Date()로 초기화
+                 isFavorite: false,
+                 text: "soososososo",
+                 photos: [
+                     Photo(id: 1, postId: 1, url: "cat1.jpg") // 여기에 사진의 URL을 적어주세요
+                 ]
+             )
+         ]
         return posts
     }
     
-    func getPostList() -> [Post] {
-        // MySQL에서 데이터 받아오는 로직
-        
-        let posts = [
-            Post(id:5, title:"자전거", price:3000, isFavorite: false, text: "hahahah"),
-            Post(id:6, title:"자전거", price:3000, isFavorite: false, text: "soososososo")
-        ]
-        return posts
+    func searchPosts(username: String, searchTerm: String, completion: @escaping (Result<[Post], Error>) -> Void) {
+        APICall.shared.get("post/search", parameters: ["username":username], queryParameters: ["searchTerm":searchTerm]) { (result: Result<[Post], Error>) in
+                switch result {
+                case .success(let posts):
+                    print("Posts retrieved successfully: \(posts.count) posts found.")
+                    completion(.success(posts))
+                case .failure(let error):
+                    print("Error fetching posts: \(error)")
+                    completion(.failure(error))
+                }
+            }
     }
     
     func getPostDetail(userId:Int) -> Post {
         // MySQL에서 데이터 받아오는 로직
         
-        let posts = Post(id:7, title:"자전거", price:3000, isFavorite: false, text: "hahahah")
+        let posts = Post(
+            id: 9,
+            title: "자전거",
+            category: 1,
+            price: 3000.0,
+            emdId: 1,
+            latitude: 37.5,
+            longitude: 160.0,
+            locationDetail: "아무데나",
+            createAt: Date(), // Date()로 초기화
+            isFavorite: false,
+            text: "soososososo",
+            photos: [
+                Photo(id: 1, postId: 1, url: "cat1.jpg") // 여기에 사진의 URL을 적어주세요
+            ]
+        )
       
         return posts
     }
@@ -50,9 +126,38 @@ class PostRepository {
         // MySQL에서 데이터 받아오는 로직
         
         let posts = [
-            Post(id:8, title:"자전거", price:3000, isFavorite: false, text: "hahahah"),
-            Post(id:9, title:"자전거", price:3000, isFavorite: false, text: "soososososo")
-        ]
+            Post(
+                 id: 9,
+                 title: "자전거",
+                 category: 1,
+                 price: 3000.0,
+                 emdId: 1,
+                 latitude: 37.5,
+                 longitude: 160.0,
+                 locationDetail: "아무데나",
+                 createAt: Date(), // Date()로 초기화
+                 isFavorite: false,
+                 text: "soososososo",
+                 photos: [
+                     Photo(id: 1, postId: 1, url: "cat1.jpg") // 여기에 사진의 URL을 적어주세요
+                 ]
+             ), Post(
+                 id: 9,
+                 title: "자전거",
+                 category: 1,
+                 price: 3000.0,
+                 emdId: 1,
+                 latitude: 37.5,
+                 longitude: 160.0,
+                 locationDetail: "아무데나",
+                 createAt: Date(), // Date()로 초기화
+                 isFavorite: false,
+                 text: "soososososo",
+                 photos: [
+                     Photo(id: 1, postId: 1, url: "cat1.jpg") // 여기에 사진의 URL을 적어주세요
+                 ]
+             )
+         ]
         return posts
     }
 }
